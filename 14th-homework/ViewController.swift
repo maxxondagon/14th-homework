6c11bd131ec4d5d67479104dfafe5e8448558b1b
Maxim Soloboev <maxxondagon@gmail.com>
2023-01-06 09:28:30 +0500

    Initial Commit

---
+++ 14th-homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  14th-homework
+//
+//  Created by Maxim Soloboev on 06.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
